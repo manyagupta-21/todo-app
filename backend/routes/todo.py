9a803bf795ab2cb0877272e118a3ff1957e980a7
manyagupta-21 <165554311+manyagupta-21@users.noreply.github.com>
2026-01-01 20:20:58 +0530

    Update todo.py

--- backend/routes/todo.py
+++ backend/routes/todo.py
@@ -1,1 +1,22 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..database import SessionLocal
+from .. import crud, schemas
+
+router = APIRouter(prefix="/todos", tags=["Todos"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/", response_model=schemas.TodoOut)
+def create(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db, todo)
+
+@router.get("/", response_model=list[schemas.TodoOut])
+def read_all(db: Session = Depends(get_db)):
+    return crud.get_all_todos(db)
 
